7e59e357f0eec6f1084b9ba81ce65f219d0550f8
Timothy Lidede <timothyliidede@gmail.com>
2020-02-25 08:02:47 +0300

    develop click function

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,7 @@ $().ready(function () {
     $("#design-1").toggle();
     $("#design-2").toggle();
   });
+  $("#develop").click(function () {
+    $("#develop-1").toggle();
+    $("#develop-2").toggle();
+  });
